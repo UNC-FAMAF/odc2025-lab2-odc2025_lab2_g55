7d512b68d11ad12f7d6af065cef09fdf20acf5ed
Juan Martín Hernández <juan5hernandez@proton.me>
2025-05-30 20:26:50 -0300

    Arreglado bug en circle.s y reducido el número de instrucciones

--- ejercicio1/circle.s
+++ ejercicio1/circle.s
@@ -1,6 +1,6 @@
-// circle_draw(cx, cy, r, filled, clr, thickness)
+// circle_draw(cx, cy, r, filled, clr)
 // Esta subrutina dibuja un círculo a la pantalla
-// Toma de argumentos X0, X1, X2, X3, X4, X5 los cuales
+// Toma de argumentos X0, X1, X2, X3, X4 los cuales
 // Deben contener:
 // X0: cx, la coordenada en el eje x del centro del círculo
 // X1: cy, la coordenada en el eje y del centro del círculo
@@ -18,12 +18,15 @@
 .extern put_pixel
 circle_draw:
     str x30, [sp, #-8]! // Pusheo x30 al stack
-    cmp x3, #0
-    b.le fill           // No se dibuja nada si thickness <= 0
 
     mov x8, #0          // x = 0
     neg x9, x2          // y = -r
     mov x10, x9         // p = -r
+
+    sub x6, x3, #1      // half_low = thickness - 1
+    lsr x6, x6, #1      // half_low = (thickness - 1) div 2
+
+    mov x7, x3, lsr #1  // half_high = thickness div 2
 
 loop_circ:
     adds xzr, x8, x9      // Seteo flags para checkear la condición del loop (x < -y)
@@ -35,10 +38,9 @@ loop_circ:
     add x9, x9, #1      // y += 1
 
     // X11 es un registro temporal para calcular 2*(x+y)+1
-    add x11, x8, x9     // X11 = x+y
-    lsl x11, x11, #1    // X11 = 2*(x+y)
-    add x11, x11, #1    // X11 = 2*(x+y)+1
-    add x10, x10, x11   // p += 2*(x+y)+1
+    add x11, x8, x9             // X11 = x+y
+    add x10, x10, x11, lsl #1   // p += 2*(x+y)
+    add x10, x10, #1            // p += 2*(x+y) + 1
     b fi                // saltea el else
 
 else:
@@ -47,69 +49,54 @@ else:
 
 fi: // fin del condicional
 
-    sub x6, x3, #1      // half_low = thickness - 1
-    lsr x6, x6, #1      // half_low = (thickness - 1) div 2
-
-    mov x7, x3, lsr #1  // half_high = thickness div 2
+    cmp x3, #0
+    b.le fill           // Si fill < 0 se dibuja el círculo sólido
 
     neg x14, x6     // dx = -half_low
 loop1:
     neg x15, x6     // dy = -half_low
 loop2:
+    add x5, x14, x0     // x5 = cx + dx
+    add x19, x15, x1    // x19 = cy + dy
+
     // put_pixel(cx + x + dx, cy + y + dy)
-    add x11, x0, x8     
-    add x11, x11, x14
-    add x12, x1, x9
-    add x12, x12, x15
+    add x11, x5, x8
+    add x12, x19, x9
     bl put_pixel
 
     // put_pixel(cx - x + dx, cy + y + dy)
-    sub x11, x0, x8
-    add x11, x11, x14
-    add x12, x1, x9
-    add x12, x12, x15
+    sub x11, x5, x8
+    add x12, x19, x9
     bl put_pixel
 
     // put_pixel(cx + x + dx, cy - y + dy)
-    add x11, x0, x8
-    add x11, x11, x14
-    sub x12, x1, x9
-    add x12, x12, x15
+    add x11, x5, x8
+    sub x12, x19, x9
     bl put_pixel
 
     // put_pixel(cx - x + dx, cy - y + dy)
-    sub x11, x0, x8
-    add x11, x11, x14
-    sub x12, x1, x9
-    add x12, x12, x15
+    sub x11, x5, x8
+    sub x12, x19, x9
     bl put_pixel
 
     // put_pixel(cx + y + dx, cy + x + dy)
-    add x11, x0, x9
-    add x11, x11, x14
-    add x12, x1, x8
-    add x12, x12, x15
+    add x11, x5, x9
+    add x12, x19, x8
     bl put_pixel
 
     // put_pixel(cx + y + dx, cy - x + dy)
-    add x11, x0, x9
-    add x11, x11, x14
-    sub x12, x1, x8
-    add x12, x12, x15
+    add x11, x5, x9
+    sub x12, x19, x8
     bl put_pixel
 
     // put_pixel(cx - y + dx, cy + x + dy)
-    sub x11, x0, x9
-    add x11, x11, x14
-    add x12, x1, x8
-    add x12, x12, x15
+    sub x11, x5, x9
+    add x12, x19, x8
     bl put_pixel
 
     // put_pixel(cx - y + dx, cy - x + dy)
-    sub x11, x0, x9
-    add x11, x11, x14
-    sub x12, x1, x8
-    add x12, x12, x15
+    sub x11, x5, x9
+    sub x12, x19, x8
     bl put_pixel
 
     add x15, x15, #1    // dy += 1
