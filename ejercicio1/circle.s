a97fe174aed8f4646c90744cdc7ae964bf1634fc
Juan Martín Hernández <juan5hernandez@proton.me>
2025-05-30 19:51:10 -0300

    Actualizada documentación en circle.s

--- ejercicio1/circle.s
+++ ejercicio1/circle.s
@@ -1,13 +1,12 @@
-// circle_draw(cx, cy, r, filled, clr)
+// circle_draw(cx, cy, r, filled, clr, thickness)
 // Esta subrutina dibuja un círculo a la pantalla
-// Toma de argumentos X0, X1, X2, X3, X4 los cuales
+// Toma de argumentos X0, X1, X2, X3, X4, X5 los cuales
 // Deben contener:
 // X0: cx, la coordenada en el eje x del centro del círculo
 // X1: cy, la coordenada en el eje y del centro del círculo
 // X2: r, el radio del círculo
-// X3: filled, si es 1 el circulo se rellena y si es 0 se dibuja solo el borde
+// X3: fill, si es <= 0 el circulo se dibuja relleno, si el valor > 0 se dibuja solo el borde y del grosor indicado
 // X4: clr, color con el que se dibuja el círculo
-// X5: thickness, Grosor con el que se va a dibujar la línea. Si X5 <= 0 no se dibuja nada
 
 // Variables de circle_draw
 // X6 = half_low
@@ -19,8 +18,8 @@
 .extern put_pixel
 circle_draw:
     str x30, [sp, #-8]! // Pusheo x30 al stack
-    cmp x5, #0
-    b.le done           // No se dibuja nada si thickness <= 0
+    cmp x3, #0
+    b.le fill           // No se dibuja nada si thickness <= 0
 
     mov x8, #0          // x = 0
     neg x9, x2          // y = -r
@@ -48,10 +47,10 @@ else:
 
 fi: // fin del condicional
 
-    sub x6, x5, #1      // half_low = thickness - 1
+    sub x6, x3, #1      // half_low = thickness - 1
     lsr x6, x6, #1      // half_low = (thickness - 1) div 2
 
-    mov x7, x5, lsr #1  // half_high = thickness div 2
+    mov x7, x3, lsr #1  // half_high = thickness div 2
 
     neg x14, x6     // dx = -half_low
 loop1:
@@ -120,7 +119,12 @@ loop2:
     add x14, x14, #1    // dx += 1
     cmp x14, x7
     b.le loop1
+    b next_loop
 
+fill:
+    //TODO
+
+next_loop:
     add x8, x8, #1  // x += 1
     b loop_circ
 
